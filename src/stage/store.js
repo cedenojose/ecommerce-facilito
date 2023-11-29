8476f0c8d02f82deadfad530a50587e886640275
Jose Cedeño <54752683+cedenojose@users.noreply.github.com>
2023-11-29 18:23:44 -0400

    change store.js se agrego un if al inicio para verificar si se encuentra disponible el api del navegador, o aun se encuentra en compilacion en el server

--- src/stage/store.js
+++ src/stage/store.js
@@ -3,13 +3,19 @@ import reducer from './reducers';
 import { load, save } from "redux-localstorage-simple";
 
 const reduxCreateStorage = () => {
+    // solucion para compilar el codigo en netlify
+    // no puede ejecutar window ya que es una propiedad del navegador y al compilar lo hace con node.js
+    let navegadorWindow = null;
+    if(typeof window !== 'undefined') {
+        navegadorWindow = window.devToolsExtension ? window.devToolsExtension() : f => f
+    }
     // el primer parametreo es el reducer, el segundo es el estado inicial del store
     // el ultimo es un middleware que pide la devtool de redux para funcionar, se puede agregar muchas mas
     return(legacy_createStore(
         reducer,
         load(),
         compose(
-            window.devToolsExtension ? window.devToolsExtension() : f => f
+            navegadorWindow
         )
     ))
 };
